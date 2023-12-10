0756078d5a28f9bec869e5c21016aa9bfca77550
Mahendra singh <mahi07ms03@gmail.com>
2023-12-10 14:49:34 +0530

    fixed a bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const carouselItems = document.querySelectorAll(".carousel_item"); 
 let i = 1;
-
+let f = 0;
 setInterval(() => {
 // Accessing All the carousel Items
  Array.from(carouselItems).forEach((item,index) => {
@@ -9,15 +9,18 @@ setInterval(() => {
     item.style.transform = `translateX(-${i*100}%)`
    }
   })
+  
+  
+  
+  if(!f)i++;
+  else i--;
 
-
-  if(i < carouselItems.length){
-    i++;
+  if(i == carouselItems.length){
+    f = 1;
+    i -= 2;
   }
-  else{
-    i=0;
-  }
-},2000)
+  if(i == 0)f = 0;
+},3000)
 
 // multiple carousel........
 
